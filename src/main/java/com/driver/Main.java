45876a8025ff4539a74ba58c66254dcc728a1029
yoges <yogeshsawlanisawlani143@gmail.com>
2023-08-14 08:59:45 +0530

    encapsulation Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+           public static void main(String[]args)
+           {
+               RWOnly rr=new RWOnly();
+              // System.out.println(rr.name="Accio");  // Error:name has private access in com.driver.RWOnly
+               rr.setName("Accio");
+               System.out.println(rr.getName());
+           }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public void setName(String name1)
+    {
+        this.name=name1;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
 }
